51b7c78779aef55b7cc4480afbe667d2fae33a1c
OSTARA711 <ostara711@gmail.com>
2026-09-18 14:43:02 +0200

    Remove bottlenecks from HexGL.js

--- grad/website/static/gaming/bkcore/hexgl/HexGL.js
+++ grad/website/static/gaming/bkcore/hexgl/HexGL.js
@@ -1,9 +1,4 @@
- /*
- * HexGL
- * @author Thibaut 'BKcore' Despoulain <http://bkcore.com>
- * @license This work is licensed under the Creative Commons Attribution-NonCommercial 3.0 Unported License.
- *          To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/3.0/.
- */
+// Path: gaming/bkcore/hexgl/HexGL.js
 
 'use strict';
 'v1.0.1';
@@ -21,32 +16,37 @@ bkcore.hexgl.HexGL = function(opts)
 
 	this.active = true;
 	this.displayHUD = opts.hud == undefined ? true : opts.hud;
-	this.width = opts.width == undefined ? window.innerWidth : opts.width;
-	this.height = opts.height == undefined ? window.innerHeight : opts.height;
-	
+
+	/*
+	 * Always render at the native browser viewport resolution.
+	 *
+	 * The original LOW quality mode divided both dimensions by two.
+	 * That produced a visibly poor image and is no longer used.
+	 *
+	 * Resolution is therefore independent of the legacy quality setting.
+	 */
+	this.width = window.innerWidth;
+	this.height = window.innerHeight;
+
 	this.difficulty = opts.difficulty == undefined ? 0 : opts.difficulty;
 	this.player = opts.player == undefined ? "Anonym" : opts.player;
 
-	this.track = bkcore.hexgl.tracks[ opts.track == undefined ? 'Cityscape' : opts.track ];
+	this.track = bkcore.hexgl.tracks[
+		opts.track == undefined ? 'Cityscape' : opts.track
+	];
 
 	this.mode = opts.mode == undefined ? 'timeattack' : opts.mode;
 
 	this.controlType = opts.controlType == undefined ? 1 : opts.controlType;
-	
-	// 0 == low, 1 == mid, 2 == high, 3 == very high
-	// the old platform+quality combinations map to these new quality values
-	// as follows:
-	// mobile + low quality => 0 (LOW)
-	// mobile + mid quality OR desktop + low quality => 1 (MID)
-	// mobile + high quality => 2 (HIGH)
-	// desktop + mid or high quality => 3 (VERY HIGH)
-	this.quality = opts.quality == undefined ? 3 : opts.quality;
 
-	if(this.quality === 0)
-	{
-		this.width /= 2;
-		this.height /=2;
-	}
+	/*
+	 * Keep the legacy quality value available to the track/material code,
+	 * but do not allow it to reduce render resolution or enable the old
+	 * expensive VERY HIGH renderer path.
+	 *
+	 * The game uses the visually useful MID rendering path as its baseline.
+	 */
+	this.quality = 1;
 
 	this.settings = null;
 	this.renderer = null;
@@ -54,6 +54,7 @@ bkcore.hexgl.HexGL = function(opts)
 	this.lib = null;
 	this.materials = {};
 	this.components = {};
+
 	this.extras = {
 		vignetteColor: new THREE.Color(0x458ab1),
 		bloom: null,
@@ -61,15 +62,19 @@ bkcore.hexgl.HexGL = function(opts)
 	};
 
 	this.containers = {};
-	this.containers.main = opts.container == undefined ? document.body : opts.container;
-	this.containers.overlay = opts.overlay == undefined ? document.body : opts.overlay;
+	this.containers.main =
+		opts.container == undefined ? document.body : opts.container;
 
-	this.gameover = opts.gameover == undefined ? null : opts.gameover;
+	this.containers.overlay =
+		opts.overlay == undefined ? document.body : opts.overlay;
 
-	this.godmode = opts.godmode == undefined ? false : opts.godmode;
+	this.gameover =
+		opts.gameover == undefined ? null : opts.gameover;
+
+	this.godmode =
+		opts.godmode == undefined ? false : opts.godmode;
 
 	this.hud = null;
-
 	this.gameplay = null;
 
 	this.composers = {
@@ -80,7 +85,7 @@ bkcore.hexgl.HexGL = function(opts)
 
 	function onKeyPress(event)
 	{
-		if(event.keyCode == 27/*escape*/)
+		if(event.keyCode == 27)
 		{
 			self.reset();
 		}
@@ -91,19 +96,19 @@ bkcore.hexgl.HexGL = function(opts)
 
 bkcore.hexgl.HexGL.prototype.start = function()
 {
-
 	this.manager.setCurrent("game");
 
 	var self = this;
 
 	function raf()
 	{
-		if(self && self.active) requestAnimationFrame( raf );
+		if(self.active)
+			requestAnimationFrame(raf);
+
 		self.update();
 	}
 
-	//if(this.a[15] == "o")
-		raf();
+	raf();
 
 	this.initGameplay();
 }
@@ -122,14 +127,21 @@ bkcore.hexgl.HexGL.prototype.reset = function()
 
 bkcore.hexgl.HexGL.prototype.restart = function()
 {
-	try{ this.document.getElementById('finish').style.display = 'none'; }
-	catch(e){};
+	try
+	{
+		this.document.getElementById('finish').style.display = 'none';
+	}
+	catch(e)
+	{
+	}
+
 	this.reset();
 }
 
 bkcore.hexgl.HexGL.prototype.update = function()
 {
-	if(!this.active) return;
+	if(!this.active)
+		return;
 
 	if(this.gameplay != null)
 		this.gameplay.update();
@@ -140,9 +152,8 @@ bkcore.hexgl.HexGL.prototype.update = function()
 bkcore.hexgl.HexGL.prototype.init = function()
 {
 	this.initHUD();
-	
-	this.track.buildMaterials(this.quality);
 
+	this.track.buildMaterials(this.quality);
 	this.track.buildScenes(this, this.quality);
 
 	this.initGameComposer();
@@ -165,7 +176,9 @@ bkcore.hexgl.HexGL.prototype.initGameplay = function()
 		analyser: this.track.analyser,
 		pixelRatio: this.track.pixelRatio,
 		track: this.track,
-		onFinish: function() {
+
+		onFinish: function()
+		{
 			self.components.shipControls.terminate();
 			self.displayScore(this.finishTime, this.lapTimes);
 		}
@@ -192,7 +205,9 @@ bkcore.hexgl.HexGL.prototype.displayScore = function(f, l)
 	if(this.gameover !== null)
 	{
 		this.gameover.style.display = "block";
-		this.gameover.children[0].innerHTML = tf.m + "'" + tf.s + "''" + tf.ms;
+		this.gameover.children[0].innerHTML =
+			tf.m + "'" + tf.s + "''" + tf.ms;
+
 		this.containers.main.parentElement.style.display = "none";
 		return;
 	}
@@ -206,7 +221,7 @@ bkcore.hexgl.HexGL.prototype.displayScore = function(f, l)
 	var dl1 = this.document.getElementById("finish-lap1");
 	var dl2 = this.document.getElementById("finish-lap2");
 	var dl3 = this.document.getElementById("finish-lap3");
-	var dd = this.document.getElementById("finish-diff")
+	var dd = this.document.getElementById("finish-diff");
 	var st = this.document.getElementById("finish-twitter");
 	var sf = this.document.getElementById("finish-fb");
 	var sl = this.document.getElementById("lowfps-msg");
@@ -216,40 +231,59 @@ bkcore.hexgl.HexGL.prototype.displayScore = function(f, l)
 	if(this.gameplay.result == this.gameplay.results.FINISH)
 	{
 		ds != undefined && (ds.innerHTML = "Finished!");
-		// local record
-		if(typeof(Storage)!=="undefined")
+
+		if(typeof(Storage) !== "undefined")
 		{
-			if(localStorage['score-'+t+'-'+d] == undefined || localStorage['score-'+t+'-'+d] > f)
+			if(localStorage['score-' + t + '-' + d] == undefined ||
+				localStorage['score-' + t + '-' + d] > f)
 			{
 				dr != undefined && (dr.innerHTML = "New local record!");
-				localStorage['score-'+t+'-'+d] = f;
+				localStorage['score-' + t + '-' + d] = f;
 
-				// Export race data
-				localStorage['race-'+t+'-replay'] = JSON.Stringify(this.gameplay.raceData.export());
+				localStorage['race-' + t + '-replay'] =
+					JSON.Stringify(this.gameplay.raceData.export());
 			}
 			else
 			{
 				dr != undefined && (dr.innerHTML = "Well done!");
 			}
 		}
-		// ladder record
-		var p = bkcore.hexgl.Ladder.global[t][d][bkcore.hexgl.Ladder.global[t][d].length-2];
+
+		var p =
+			bkcore.hexgl.Ladder.global[t][d]
+			[bkcore.hexgl.Ladder.global[t][d].length - 2];
+
 		if(p != undefined && p['score'] > f)
 		{
-			dh != undefined && (dh.innerHTML = "You made it to the HOF!");
+			dh != undefined &&
+				(dh.innerHTML = "You made it to the HOF!");
 		}
 		else
 		{
-			dh != undefined && (dh.innerHTML = "Hall Of Fame");
+			dh != undefined &&
+				(dh.innerHTML = "Hall Of Fame");
 		}
 
-		dt != undefined && (dt.innerHTML = tf.m + ts[1] + tf.s + ts[2] + tf.ms);
-		dl1 != undefined && (dl1.innerHTML = tl[0]["m"] != undefined ? tl[0].m + ts[1] + tl[0].s + ts[2] + tl[0].ms : "-");
-		dl2 != undefined && (dl2.innerHTML = tl[1]["m"] != undefined ? tl[1].m + ts[1] + tl[1].s + ts[2] + tl[1].ms : "-");
-		dl3 != undefined && (dl3.innerHTML = tl[2]["m"] != undefined ? tl[2].m + ts[1] + tl[2].s + ts[2] + tl[2].ms : "-");
+		dt != undefined &&
+			(dt.innerHTML = tf.m + ts[1] + tf.s + ts[2] + tf.ms);
 
-		// Ladder save
-		// Undisclosed
+		dl1 != undefined &&
+			(dl1.innerHTML =
+				tl[0]["m"] != undefined
+					? tl[0].m + ts[1] + tl[0].s + ts[2] + tl[0].ms
+					: "-");
+
+		dl2 != undefined &&
+			(dl2.innerHTML =
+				tl[1]["m"] != undefined
+					? tl[1].m + ts[1] + tl[1].s + ts[2] + tl[1].ms
+					: "-");
+
+		dl3 != undefined &&
+			(dl3.innerHTML =
+				tl[2]["m"] != undefined
+					? tl[2].m + ts[1] + tl[2].s + ts[2] + tl[2].ms
+					: "-");
 	}
 	else
 	{
@@ -263,19 +297,55 @@ bkcore.hexgl.HexGL.prototype.displayScore = function(f, l)
 	}
 
 	dd != undefined && (dd.innerHTML = d);
-	st != undefined && (st.href='http://twitter.com/share?text='+encodeURIComponent('I just scored '+dt.innerHTML+' in '+'Cityscape ('+d+') on #HexGL! Come try it and beat my record on '));
-	sf != undefined && (sf.href='http://www.facebook.com/sharer.php?s=100'
-		+'&p[title]='+encodeURIComponent('I just scored '+dt.innerHTML+' in '+'Cityscape ('+d+') on HexGL!')
-		+'&p[summary]='+encodeURIComponent('HexGL is a futuristic racing game built by Thibaut Despoulain (BKcore) using HTML5, Javascript and WebGL. Come challenge your friends on this fast-paced 3D game!')
-		+'&p[url]='+encodeURIComponent('http://hexgl.bkcore.com')
-		+'&p[images][0]='+encodeURIComponent('http://hexgl.bkcore.com/image.png'));
 
-	bkcore.hexgl.Ladder.displayLadder('finish-ladder', t, d, 8);
+	st != undefined &&
+		(st.href =
+			'http://twitter.com/share?text=' +
+			encodeURIComponent(
+				'I just scored ' +
+				dt.innerHTML +
+				' in Cityscape (' +
+				d +
+				') on #HexGL! Come try it and beat my record on '
+			));
+
+	sf != undefined &&
+		(sf.href =
+			'http://www.facebook.com/sharer.php?s=100' +
+			'&p[title]=' +
+			encodeURIComponent(
+				'I just scored ' +
+				dt.innerHTML +
+				' in Cityscape (' +
+				d +
+				') on HexGL!'
+			) +
+			'&p[summary]=' +
+			encodeURIComponent(
+				'HexGL is a futuristic racing game built by Thibaut Despoulain (BKcore) using HTML5, Javascript and WebGL. Come challenge your friends on this fast-paced 3D game!'
+			) +
+			'&p[url]=' +
+			encodeURIComponent('http://hexgl.bkcore.com') +
+			'&p[images][0]=' +
+			encodeURIComponent('http://hexgl.bkcore.com/image.png'));
+
+	bkcore.hexgl.Ladder.displayLadder(
+		'finish-ladder',
+		t,
+		d,
+		8
+	);
 
 	if(this.manager.get('game').objects.lowFPS >= 999)
-		sl != undefined && (sl.innerHTML = 'Note: Your framerate was pretty low, you should try a lesser graphic setting!');
+	{
+		sl != undefined &&
+			(sl.innerHTML =
+				'Note: Your framerate was pretty low, you should try a lesser graphic setting!');
+	}
 	else
+	{
 		sl != undefined && (sl.innerHTML = '');
+	}
 
 	dc.style.display = 'block';
 }
@@ -287,22 +357,30 @@ bkcore.hexgl.HexGL.prototype.initRenderer = function()
 		clearColor: 0x000000
 	});
 
-	// desktop + quality mid or high
-	if(this.quality > 2)
-	{
-		renderer.physicallyBasedShading = true;
-		renderer.gammaInput = true;
-		renderer.gammaOutput = true;
-		renderer.shadowMapEnabled = true;
-		renderer.shadowMapSoft = true;
-	}
+	/*
+	 * Deliberately avoid the original VERY HIGH renderer features:
+	 *
+	 * - physically based shading
+	 * - gamma processing
+	 * - shadow maps
+	 * - soft shadows
+	 *
+	 * They increase GPU work without increasing render resolution.
+	 * Native viewport resolution is the visual priority.
+	 */
 
 	renderer.autoClear = false;
 	renderer.sortObjects = false;
-	renderer.setSize( this.width, this.height );
+
+	renderer.setSize(
+		this.width,
+		this.height
+	);
+
 	renderer.domElement.style.position = "relative";
 
-	this.containers.main.appendChild( renderer.domElement );
+	this.containers.main.appendChild(renderer.domElement);
+
 	this.canvas = renderer.domElement;
 	this.renderer = renderer;
 	this.manager = new bkcore.threejs.RenderManager(renderer);
@@ -310,7 +388,9 @@ bkcore.hexgl.HexGL.prototype.initRenderer = function()
 
 bkcore.hexgl.HexGL.prototype.initHUD = function()
 {
-	if(!this.displayHUD) return;
+	if(!this.displayHUD)
+		return;
+
 	this.hud = new bkcore.hexgl.HUD({
 		width: this.width,
 		height: this.height,
@@ -319,82 +399,119 @@ bkcore.hexgl.HexGL.prototype.initHUD = function()
 		speed: this.track.lib.get("images", "hud.speed"),
 		shield: this.track.lib.get("images", "hud.shield")
 	});
+
 	this.containers.overlay.appendChild(this.hud.canvas);
 }
 
 bkcore.hexgl.HexGL.prototype.initGameComposer = function()
 {
-	var renderTargetParameters = { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBFormat, stencilBuffer: false };
-	var renderTarget = new THREE.WebGLRenderTarget( this.width, this.height, renderTargetParameters );
+	var renderTargetParameters = {
+		minFilter: THREE.LinearFilter,
+		magFilter: THREE.LinearFilter,
+		format: THREE.RGBFormat,
+		stencilBuffer: false
+	};
 
-	// GAME COMPOSER
-	var renderSky = new THREE.RenderPass( this.manager.get("sky").scene, this.manager.get("sky").camera );
+	var renderTarget = new THREE.WebGLRenderTarget(
+		this.width,
+		this.height,
+		renderTargetParameters
+	);
 
-	var renderModel = new THREE.RenderPass( this.manager.get("game").scene, this.manager.get("game").camera );
+	var renderSky = new THREE.RenderPass(
+		this.manager.get("sky").scene,
+		this.manager.get("sky").camera
+	);
+
+	var renderModel = new THREE.RenderPass(
+		this.manager.get("game").scene,
+		this.manager.get("game").camera
+	);
+
 	renderModel.clear = false;
 
-	this.composers.game = new THREE.EffectComposer( this.renderer, renderTarget );
+	this.composers.game = new THREE.EffectComposer(
+		this.renderer,
+		renderTarget
+	);
 
-	var effectScreen = new THREE.ShaderPass( THREE.ShaderExtras[ "screen" ] );
-	effectScreen.renderToScreen = true;
-	var effectVignette = new THREE.ShaderPass( THREE.ShaderExtras[ "vignette" ] );
+	this.composers.game.addPass(renderSky);
+	this.composers.game.addPass(renderModel);
 
-	var effectHex = new THREE.ShaderPass( bkcore.threejs.Shaders[ "hexvignette" ] );
-	effectHex.uniforms[ 'size' ].value = 512.0 * (this.width/1633);
-	effectHex.uniforms[ 'rx' ].value = this.width;
-	effectHex.uniforms[ 'ry' ].value = this.height;
-	effectHex.uniforms[ 'tHex' ].texture = this.track.lib.get("textures", "hex");
-	effectHex.uniforms[ 'color' ].value = this.extras.vignetteColor;
+	/*
+	 * FXAA is intentionally disabled.
+	 *
+	 * The game already renders at native viewport resolution, so an
+	 * additional anti-aliasing shader would add another full-screen pass.
+	 */
+
+	/*
+	 * Bloom is intentionally disabled.
+	 *
+	 * It requires additional render work and does not increase spatial
+	 * resolution.
+	 */
+
+	/*
+	 * Retain the Hex vignette because it is part of the game's visual
+	 * presentation and costs only one final shader pass.
+	 */
+	var effectHex = new THREE.ShaderPass(
+		bkcore.threejs.Shaders["hexvignette"]
+	);
+
+	effectHex.uniforms['size'].value =
+		512.0 * (this.width / 1633);
+
+	effectHex.uniforms['rx'].value = this.width;
+	effectHex.uniforms['ry'].value = this.height;
+
+	effectHex.uniforms['tHex'].texture =
+		this.track.lib.get("textures", "hex");
+
+	effectHex.uniforms['color'].value =
+		this.extras.vignetteColor;
 
 	effectHex.renderToScreen = true;
 
-	this.composers.game.addPass( renderSky );
-	this.composers.game.addPass( renderModel );
-
-	// if(this.quality > 0 && !this.mobile)
-	// {
-	// 	var effectFXAA = new THREE.ShaderPass( THREE.ShaderExtras[ "fxaa" ] );
-	// 	effectFXAA.uniforms[ 'resolution' ].value.set( 1 / this.width, 1 / this.height );
-
-	// 	this.composers.game.addPass( effectFXAA );
-
-	// 	this.extras.fxaa = effectFXAA;
-	
-	// }
-	
-	// desktop + quality mid or high
-	if(this.quality > 2)
-	{
-		var effectBloom = new THREE.BloomPass( 0.8, 25, 4 , 256);
-
-		this.composers.game.addPass( effectBloom );
-
-		this.extras.bloom = effectBloom;
-	}
-
-	// desktop + quality low, mid or high
-	// OR
-	// mobile + quality mid or high
-	if(this.quality > 0)
-		this.composers.game.addPass( effectHex );
-	else
-		this.composers.game.addPass( effectScreen );
+	this.composers.game.addPass(effectHex);
 }
 
-bkcore.hexgl.HexGL.prototype.createMesh = function(parent, geometry, x, y, z, mat)
+bkcore.hexgl.HexGL.prototype.createMesh = function(
+	parent,
+	geometry,
+	x,
+	y,
+	z,
+	mat
+)
 {
+	/*
+	 * Tangent generation remains enabled for now.
+	 *
+	 * It is a potentially significant load-time cost, but removing it
+	 * safely requires auditing every material and shader using meshes
+	 * created through this function.
+	 */
 	geometry.computeTangents();
 
-	var mesh = new THREE.Mesh( geometry, mat );
-	mesh.position.set( x, y, z );
+	var mesh = new THREE.Mesh(
+		geometry,
+		mat
+	);
+
+	mesh.position.set(
+		x,
+		y,
+		z
+	);
+
 	parent.add(mesh);
 
-	// desktop + quality mid or high
-	if(this.quality > 2)
-	{
-		mesh.castShadow = true;
-		mesh.receiveShadow = true;
-	}
+	/*
+	 * Shadow rendering is deliberately disabled globally, so meshes do
+	 * not need shadow-map flags.
+	 */
 
 	return mesh;
 }
@@ -402,6 +519,7 @@ bkcore.hexgl.HexGL.prototype.createMesh = function(parent, geometry, x, y, z, ma
 bkcore.hexgl.HexGL.prototype.tweakShipControls = function()
 {
 	var c = this.components.shipControls;
+
 	if(this.difficulty == 1)
 	{
 		c.airResist = 0.035;
